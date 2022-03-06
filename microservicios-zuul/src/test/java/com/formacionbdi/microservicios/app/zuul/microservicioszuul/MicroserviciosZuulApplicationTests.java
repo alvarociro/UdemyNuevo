a7fcb76590363ecf28419c835453a77e059cd0f2
Alvaro Ciro <acasaverde13@gmail.com>
2022-03-06 17:41:33 -0500

    eureka + zuul + micro_usuarios correcto probado con postman

---
+++ microservicios-zuul/src/test/java/com/formacionbdi/microservicios/app/zuul/microservicioszuul/MicroserviciosZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.microservicios.app.zuul.microservicioszuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
